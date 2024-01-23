c19170aeec1c3718a0c35a2f0fbe44d187460abe
kunal7479 <99002313+kunal7479@users.noreply.github.com>
2024-01-23 17:14:02 +0530

    Add files via upload

---
+++ AzureAssignment/AzureAssignment/src/main/java/com/azureassignment/AzureAssignment/AzureAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.azureassignment.AzureAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureAssignmentApplication.class, args);
+	}
+
+}
---
+++ AzureAssignment/AzureAssignment/src/main/java/com/azureassignment/AzureAssignment/controller/Controller.java
@@ -0,0 +1,16 @@
+package com.azureassignment.AzureAssignment.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/azure")
+public class Controller {
+
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello Kunal";
+	}
+}
